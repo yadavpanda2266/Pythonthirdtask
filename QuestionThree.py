54fee3e5d4e772dcc2f3113b5f7e5c1645f7cab9
yadavpanda2266 <yadavpanda2266@gmail.com>
2025-07-26 21:06:50 +0530

    Add files via upload

---
+++ QuestionThree.py
@@ -0,0 +1,17 @@
+#Factorial number
+'''n = int(input("Enter your no : "))
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n*(factorial(n-1))
+result = factorial(n)
+print("Factorial no is : ",result)'''
+
+n = int(input("Enter your Number : "))
+
+from  math import *
+print(sqrt(n))
+print(log(n))
+print(radians(n))
+
